6b45952729a017809e6de91f60d623a3ffad8082
princeantwi2k <antwi2k@gmail.com>
2021-12-21 12:39:43 -0500

    added content

---
+++ mines.js
@@ -0,0 +1,25 @@
+
+console.log(typeof("form--hidden"))
+console.log(typeof(0.342))
+console.log(typeof(the))
+console.log(typeof("the"))
+console.log(typeof(value)==='date')
+
+
+let reaction ="10";
+console.log(typeof(reaction));
+reaction = "dog";
+
+
+let arr =[222,7,99,0];
+let str = "hello";
+
+
+arr[0]=876;
+str [0]= "2";
+
+
+
+   console.log(arr)
+   console.log(str)
+   
